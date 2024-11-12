6cfcd0e9af798fd9b7d237f951d4b874977d495c
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-11-12 19:11:11 +0000

    Better debug flag

--- make/flags.mk
+++ make/flags.mk
@@ -1,6 +1,6 @@
 CC := gcc
 ifeq ($(DEBUG), 1)
-  CC_FLAGS := -ggdb3 -MD -MP -O2 -DDEBUG -D_FORTIFY_SOURCE=2 -fno-common -fstack-protector-strong
+  CC_FLAGS := -ggdb3 -Og -MD -MP -DDEBUG -D_FORTIFY_SOURCE=2 -fno-common -fstack-protector-strong
 else
   CC_FLAGS := -s -O3 -DNDEBUG -fno-common -fstack-protector-strong -ffunction-sections -fdata-sections -flto=auto -Wl,--gc-sections
 endif
