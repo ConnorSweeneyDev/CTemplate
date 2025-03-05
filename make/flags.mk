ca2b89f8c3f7080775849139a62312fad52aa985
Connor Sweeney <connor.sweeney1234@gmail.com>
2025-03-05 12:17:29 +0000

    Better build flags and make output

--- make/flags.mk
+++ make/flags.mk
@@ -2,7 +2,7 @@ CC := gcc
 ifeq ($(DEBUG), 1)
   CC_FLAGS := -ggdb3 -Og -MD -MP -DDEBUG -D_FORTIFY_SOURCE=2 -fno-common -fstack-protector-strong
 else
-  CC_FLAGS := -s -O3 -DNDEBUG -fno-common -fstack-protector-strong -ffunction-sections -fdata-sections -flto=auto -Wl,--gc-sections
+  CC_FLAGS := -s -O3 -DNDEBUG -fno-common -fstack-protector-strong
 endif
 
 WARNINGS := -Wall -Wextra -Wpedantic -Wconversion -Wshadow -Wundef -Wclobbered -Wdeprecated -Wmultichar -Wuninitialized -Wunreachable-code -Wstrict-aliasing -Wreturn-type -Wtype-limits -Wformat-security -Wpointer-arith -Wmaybe-uninitialized -Wempty-body -Wdouble-promotion -Wcast-qual -Wcast-align -Wfloat-equal -Wlogical-op -Wduplicated-cond -Wshift-overflow=2 -Wformat=2
